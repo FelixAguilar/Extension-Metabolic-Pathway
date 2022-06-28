947c5ea3f7b7d287b5525a5f0a41566a7e4a5231
Felix <aguilarferrerfelix@gmail.com>
2022-06-28 17:03:14 +0200

    mbb finished

--- Metabolic_Pathway/New_Element.py
+++ Metabolic_Pathway/New_Element.py
@@ -3,13 +3,12 @@ from wsgiref.util import request_uri
 import inkex
 from inkex import TextElement, PathElement
 from re import S
-from math import pi
 
 # returns value as svg format.
 def svg_format(self, value, unit):
     return self.svg.unittouu(str(value) + unit)
 
-# Adds a text label to the given location and size.
+# Adds a text label to the given id, location and size.
 def add_text(id, x, y, size, text):
     elem = TextElement(x=str(x), y=str(y), id=id)
     elem.text = str(text)
@@ -21,39 +20,20 @@ def add_text(id, x, y, size, text):
         'font-style': 'normal' }
     return elem
 
-def add_elipse(cx, cy, r):
-    style = {'stroke': 'black', 'fill': 'lightgrey', 'stroke-width': '1px'}
+# Adds a elipse to the given center position and radius.
+def add_elipse(cx, cy, r, color):
+    style = {'stroke': 'black', 'fill': color, 'stroke-width': '0.25px'}
     elem = PathElement.arc((cx, cy), r)
     elem.style = style
     return elem
 
-
-# creates new reaction.
-def add_reaction(self):
-    x = 40
-    y = x
-    size = 10
-
-    # values to SVG format.
-    layer = self.svg.get_current_layer()
-    svg_size = svg_format(self , size, 'pt')
-    svg_x = svg_format(self, x, 'px')
-    svg_y = svg_format(self, y, 'px')
-
-    layer.add(add_text(svg_x, svg_y, svg_size, self.options.ID))
-    y += 15
-    svg_y = svg_format(self, y, 'px')
-    layer.add(add_text(svg_x, svg_y, svg_size, self.options.KEGG_reaction))
-    y += 15
-    svg_y = svg_format(self, y, 'px')
-    layer.add(add_text(svg_x, svg_y, svg_size, self.options.KEGG_enzime))
-
+# Generates a metabolic bujil
 def add_metabolic_building_block(self):
 
     id = self.options.ID_M
-    x = 40
-    y = 40
-    r = 25
+    x = self.options.x_M
+    y = self.options.y_M
+    r = self.options.size_M
     s = 10
     
     svg_x = svg_format(self, x, 'px')
@@ -64,10 +44,30 @@ def add_metabolic_building_block(self):
     layer = self.svg.get_current_layer()
 
     mbb = inkex.Group()
-    mbb.add(add_elipse(svg_x, svg_y, svg_r))
-    mbb.add(add_text("mbb" + id, svg_x - 4, svg_y, svg_s, 'MBB'))
-    mbb.add(add_text("id" + id, svg_x - len(id), svg_y + 5, svg_s, id))
+    mbb.add(add_elipse(svg_x, svg_y, svg_r, 'lightgray'))
+    mbb.add(add_text("mbb" + id, svg_x - 4, svg_y - 1, svg_s, 'MBB'))
+    mbb.add(add_text("id" + id, svg_x - len(id), svg_y + 4, svg_s, id))
+    layer.add(mbb)
 
+# creates new reaction.
+def add_reaction(self):
+    id = self.options.ID_M
+    x = self.options.x_M
+    y = self.options.y_M
+    r = self.options.size_M
+    s = 10
+    
+    svg_x = svg_format(self, x, 'px')
+    svg_y = svg_format(self, y, 'px')
+    svg_r = svg_format(self, r, 'pt')
+    svg_s = svg_format(self, s, 'pt')
+
+    layer = self.svg.get_current_layer()
+
+    mbb = inkex.Group()
+    mbb.add(add_elipse(svg_x, svg_y, svg_r, 'yellow'))
+    mbb.add(add_text("mbb" + id, svg_x - 4, svg_y - 1, svg_s, 'MBB'))
+    mbb.add(add_text("id" + id, svg_x - len(id), svg_y + 4, svg_s, id))
     layer.add(mbb)
 
 class Constructor(inkex.EffectExtension):
@@ -81,6 +81,9 @@ class Constructor(inkex.EffectExtension):
         pars.add_argument('--type_M', type=str, default='undefined', dest='type_M', help="Type of element MDAG")
         pars.add_argument('--type_R', type=str, default='undefined', dest='type_R', help="Type of element RC")
         pars.add_argument('--tab', type=str, default='undefined', dest='tab', help="Type of Metabolic Pathway")
+        pars.add_argument('--x_M', type=int, default='30', dest='x_M', help="Position x of the element MDAG")
+        pars.add_argument('--y_M', type=int, default='30', dest='y_M', help="Position y of the element MDAG")
+        pars.add_argument('--size_M', type=int, default='20', dest='size_M', help="Size of the element MDAG")
 
     def effect(self):
         if self.options.tab == 'DAG':
