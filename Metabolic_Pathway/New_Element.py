8293074aa8c69cccf8490939ea3a09b0b8de85ee
Felix <aguilarferrerfelix@gmail.com>
2022-07-03 12:03:12 +0200

    finished programming new_Element

--- Metabolic_Pathway/New_Element.py
+++ Metabolic_Pathway/New_Element.py
@@ -1,18 +1,20 @@
 from operator import length_hint
+from tokenize import group
 from wsgiref.util import request_uri
-import inkex
+import inkex, re
 from inkex import TextElement, PathElement, Rectangle
 from re import S
 
+# Font size for all text created by the extension.
 font_size = 10
 
-# returns value as svg format.
+# Returns input value and unit as svg format.
 def svg_format(self, value, unit):
     return self.svg.unittouu(str(value) + unit)
 
 # Adds a text label to the given id, location and size.
-def add_text(id, x, y, size, text):
-    elem = TextElement(x=str(x), y=str(y), id=id)
+def add_text(x, y, size, text):
+    elem = TextElement(x=str(x), y=str(y))
     elem.text = str(text)
     elem.style = {
         'font-size': size,
@@ -42,52 +44,50 @@ def add_octogon(cx, cy, radius, color):
 # Adds a rectangle to the given center position, high and base.
 def add_rectangle(cx, cy, base, hight):
     style = {'stroke': 'black', 'fill': 'none', 'stroke-width': '0.25px'}
-    elem = Rectangle(cx - 5, cy - 5, 10, 10)
+    elem = Rectangle()
+    elem.set('x', cx)
+    elem.set('y', cy)
+    elem.set('height', base)
+    elem.set('width', hight)
     elem.style = style
     return elem
 
 # Generates a new metabolic building block in the current layer.
 def add_metabolic_building_block(self, id, x, y, radius):
 
-    # Minimum size of component is 20.
-    if(radius < 20):
-        radius = 20
+    # Minimum size of component is 6.
+    if(radius < 6):
+        radius = 6
     
-    # Change variables to svg format.
-    svg_x = svg_format(self, x, 'px')
-    svg_y = svg_format(self, y, 'px')
-    svg_r = svg_format(self, radius, 'pt')
-    svg_s = svg_format(self, font_size, 'pt')
+    # Change font size format to svg.
+    svg_font_size = svg_format(self, font_size, 'pt')
 
     # Creates a group and adds all components to it.
-    mbb = inkex.Group()
-    mbb.add(add_elipse(svg_x, svg_y, svg_r, 'lightgray'))
-    mbb.add(add_text("mbb" + id, svg_x - 4, svg_y - 1, svg_s, 'MBB'))
-    mbb.add(add_text("id" + id, svg_x - len(id), svg_y + 4, svg_s, id))
+    group = inkex.Group(id = id)
+    group.add(add_elipse(x, y, radius, 'lightgray'))
+    group.add(add_text(x - 4, y - 1, svg_font_size, 'MBB'))
+    group.add(add_text(x - len(id), y + 4, svg_font_size, id))
 
     # Adds it to the current layer.
     layer = self.svg.get_current_layer()
-    layer.add(mbb)
+    layer.add(group)
 
 # Generates a new reaction in the current layer.
 def add_reaction(self, id, reaction, enzime, x, y, radius):
 
-    # Minimum size of component is 25.
-    if(radius < 25):
-        radius = 25
+    # Minimum size of component is 9.
+    if(radius < 9):
+       radius = 9
     
-    # Change variables to svg format.
-    svg_x = svg_format(self, x, 'px')
-    svg_y = svg_format(self, y, 'px')
-    svg_r = svg_format(self, radius, 'pt')
-    svg_s = svg_format(self, font_size, 'pt')
+    # Change font size format to svg.
+    svg_font_size = svg_format(self, font_size, 'pt')
 
     # Creates a group and adds all components to it.
-    reac = inkex.Group()
-    reac.add(add_elipse(svg_x, svg_y, svg_r, 'yellow'))
-    reac.add(add_text('id' + id, svg_x - len(id), svg_y - 3, svg_s, id))
-    reac.add(add_text('reaction' + id, svg_x - len(reaction) - 1, svg_y + 1, svg_s, 'R' + reaction))
-    reac.add(add_text("enzime" + id, svg_x - len(enzime), svg_y + 5, svg_s, enzime))
+    reac = inkex.Group(id = id)
+    reac.add(add_elipse(x, y, radius, 'yellow'))
+    reac.add(add_text(x - len(id), y - 3, svg_font_size, id))
+    reac.add(add_text(x - len(reaction) - 2, y + 1, svg_font_size, 'R' + reaction))
+    reac.add(add_text(x - len(enzime), y + 5, svg_font_size, enzime))
 
     # Adds it to the current layer.
     layer = self.svg.get_current_layer()
@@ -96,60 +96,79 @@ def add_reaction(self, id, reaction, enzime, x, y, radius):
 # Generates a new inverse reaction in the current layer.
 def add_inverse_reaction(self, id, reaction, enzime, x, y, radius):
 
-    # Minimum size of component is 32.
-    if(radius < 32):
-        radius = 32
+    # Minimum size of component is 11.
+    if(radius < 11):
+        radius = 11
     
-    # Change variables to svg format.
-    svg_x = svg_format(self, x, 'px')
-    svg_y = svg_format(self, y, 'px')
-    svg_r = svg_format(self, radius, 'pt')
-    svg_s = svg_format(self, font_size, 'pt')
+    # Change font size format to svg.
+    svg_font_size = svg_format(self, font_size, 'pt')
 
     # Creates a group and adds all components to it.
-    reac = inkex.Group()
-    reac.add(add_elipse(svg_x, svg_y, svg_r, '#c1b2ff'))
-    reac.add(add_text('id' + id, svg_x - len(id), svg_y - 3, svg_s, id))
-    reac.add(add_text('reaction' + id, svg_x - len(reaction) - 5, svg_y + 1, svg_s, 'R' + reaction + '_rev'))
-    reac.add(add_text("enzime" + id, svg_x - len(enzime), svg_y + 5, svg_s, enzime))
+    group = inkex.Group(id = id)
+    group.add(add_elipse(x, y, radius, '#c1b2ff'))
+    group.add(add_text(x - len(id), y - 3, svg_font_size, id))
+    group.add(add_text(x - len(reaction) - 5, y + 1, svg_font_size, 'R' + reaction + '_rev'))
+    group.add(add_text(x - len(enzime), y + 5, svg_font_size, enzime))
 
     # Adds it to the current layer.
     layer = self.svg.get_current_layer()
-    layer.add(reac)
+    layer.add(group)
 
 # Generates a new elemental reaction in the current layer.
-def add_elemental_reaction(self, id, reaction, enzime, x, y, r):
-    if(r < 25):
-        r = 25
+def add_elemental_reaction(self, id, reaction, enzime, x, y, radius):
+
+    # Minimum radius is 10.
+    if(radius < 10):
+        radius = 10
     
-    svg_x = svg_format(self, x, 'px')
-    svg_y = svg_format(self, y, 'px')
-    svg_r = svg_format(self, r, 'pt')
-    svg_s = svg_format(self, font_size, 'pt')
+    # Change font size format to svg.
+    svg_font_size = svg_format(self, font_size, 'pt')
 
+    # Creates a group and adds all components to it.
+    group = inkex.Group(id = id)
+    group.add(add_octogon(x, y, radius, 'yellow'))
+    group.add(add_octogon(x, y, radius + 1, 'none'))
+    group.add(add_text(x - len(id), y - 3, svg_font_size, id))
+    group.add(add_text(x - len(reaction) - 2, y + 1, svg_font_size, 'R' + reaction))
+    group.add(add_text(x - len(enzime), y + 5, svg_font_size, enzime))
+
+    # Adds it to the current layer.
     layer = self.svg.get_current_layer()
+    layer.add(group)
 
-    reac = inkex.Group()
-    reac.add(add_octogon(svg_x, svg_y, svg_r, 'yellow'))
-    reac.add(add_octogon(svg_x, svg_y, svg_r + 1, 'none'))
-    reac.add(add_text('id' + id, svg_x - len(id), svg_y - 3, svg_s, id))
-    reac.add(add_text('reaction' + id, svg_x - len(reaction) - 1, svg_y + 1, svg_s, 'R' + reaction))
-    reac.add(add_text("enzime" + id, svg_x - len(enzime), svg_y + 5, svg_s, enzime))
-    layer.add(reac)
+# Generates a new component in the current layer.
+def add_component(self, component, x, y, size):
 
-def add_component(self, component, x, y, r):
-    svg_x = svg_format(self, x, 'px')
-    svg_y = svg_format(self, y, 'px')
-    svg_r = svg_format(self, r, 'pt')
-    svg_s = svg_format(self, font_size, 'pt')
+    # Minimum size for a component.
+    if(size < 13):
+        size = 13
+    
+    # Format of the font size.
+    svg_font_size = svg_format(self, font_size, 'pt')
 
+    # Creates a group with all the elements inside of it.
+    group = inkex.Group()
+    group.add(add_rectangle(x - (size/2), y - (size/6), size/3, size))
+    group.add(add_text(x - len(component) - 2, y + 1 , svg_font_size, 'C' + component))
+
+    # Gets the current layer and adds the group to it.
     layer = self.svg.get_current_layer()
+    layer.add(group)
 
-    reac = inkex.Group()
-    reac.add(add_rectangle(svg_x, svg_y, svg_r, svg_r))
-    reac.add(add_text('component' + component, svg_x - len(component), svg_y - 3, svg_s, 'C' + component))
-    layer.add(reac)
+# Checks if the id is not used in the svg.
+def check_unique_id(self, id):
 
+    # Patern that verifies it is a number.
+    pattern = re.compile("[0-9]+")
+
+    # Gets all ids in the svg.
+    svg_ids = self.svg.get_ids()
+
+    # Iterates through all the ids for a equal id.
+    for svg_id in svg_ids:
+        if(pattern.match(svg_id) and svg_id == id):
+            return False
+    return True
 
 class Constructor(inkex.EffectExtension):
     def add_arguments(self, pars):
@@ -170,6 +189,7 @@ class Constructor(inkex.EffectExtension):
         pars.add_argument('--size_R', type=int, default='20', dest='size_R', help="Size of the element RC")
 
     def effect(self):
+
         if self.options.tab == 'DAG':
             if self.options.type_M == 'Reactions':
 
@@ -177,21 +197,30 @@ class Constructor(inkex.EffectExtension):
                 if (self.options.ID_M == 'undefined' or self.options.KEGG_reaction_M == 'undefined' or self.options.KEGG_enzime_M == 'undefined'):
                     inkex.errormsg('ID, reaction or enzime code has not been defined for the reaction.')
                 else:
-                    add_reaction(self, self.options.ID_M, self.options.KEGG_reaction_M, self.options.KEGG_enzime_M, self.options.x_M, self.options.y_M, self.options.size_M)
+                    if(not check_unique_id(self, self.options.ID_M)):
+                        inkex.errormsg('The ID is already in use, please change it to another number.')
+                    else:    
+                        add_reaction(self, self.options.ID_M, self.options.KEGG_reaction_M, self.options.KEGG_enzime_M, self.options.x_M, self.options.y_M, self.options.size_M)
             elif self.options.type_M == 'Elemental_Reactions':
 
                 # New elemental reaction for MDAG.
                 if (self.options.ID_M == 'undefined' or self.options.KEGG_reaction_M == 'undefined' or self.options.KEGG_enzime_M == 'undefined'):
                     inkex.errormsg('ID, reaction or enzime code has not been defined for the elemental reaction.')
                 else:
-                    add_elemental_reaction(self, self.options.ID_M, self.options.KEGG_reaction_M, self.options.KEGG_enzime_M, self.options.x_M, self.options.y_M, self.options.size_M)
+                    if(not check_unique_id(self, self.options.ID_M)):
+                        inkex.errormsg('The ID is already in use, please change it to another number.')
+                    else:   
+                        add_elemental_reaction(self, self.options.ID_M, self.options.KEGG_reaction_M, self.options.KEGG_enzime_M, self.options.x_M, self.options.y_M, self.options.size_M)
             else:
 
                 # New metabolic building block for MDAG.
                 if(self.options.ID_M == 'undefined'):
                     inkex.errormsg('ID for metabolic building block has not been defined.')
                 else:
-                    add_metabolic_building_block(self, self.options.ID_M, self.options.x_M, self.options.y_M, self.options.size_M)
+                    if(not check_unique_id(self, self.options.ID_M)):
+                        inkex.errormsg('The ID is already in use, please change it to another number.')
+                    else:   
+                        add_metabolic_building_block(self, self.options.ID_M, self.options.x_M, self.options.y_M, self.options.size_M)
         else:
             if self.options.type_R == 'Reactions':
 
@@ -199,7 +228,10 @@ class Constructor(inkex.EffectExtension):
                 if (self.options.ID_R == 'undefined' or self.options.KEGG_reaction_R == 'undefined' or self.options.KEGG_enzime_R == 'undefined'):
                     inkex.errormsg('ID, reaction or enzime code has not been defined for the reaction.')
                 else:
-                    add_reaction(self, self.options.ID_R, self.options.KEGG_reaction_R, self.options.KEGG_enzime_R, self.options.x_R, self.options.y_R, self.options.size_R)
+                    if(not check_unique_id(self, self.options.ID_R)):
+                        inkex.errormsg('The ID is already in use, please change it to another number.')
+                    else:   
+                        add_reaction(self, self.options.ID_R, self.options.KEGG_reaction_R, self.options.KEGG_enzime_R, self.options.x_R, self.options.y_R, self.options.size_R)
             elif self.options.type_R == 'Component':
 
                 # New component for RC.
@@ -208,11 +240,15 @@ class Constructor(inkex.EffectExtension):
                 else:
                     add_component(self, self.options.KEGG_reaction_R, self.options.x_R, self.options.y_R, self.options.size_R)
             else:
+                
                 # New inverse reaction for RC.
                 if (self.options.ID_R == 'undefined' or self.options.KEGG_reaction_R == 'undefined' or self.options.KEGG_enzime_R == 'undefined'):
                     inkex.errormsg('ID, reaction or enzime code has not been defined for the inverse reaction.')
                 else:
-                    add_inverse_reaction(self, self.options.ID_R, self.options.KEGG_reaction_R, self.options.KEGG_enzime_R, self.options.x_R, self.options.y_R, self.options.size_R)
+                    if(not check_unique_id(self, self.options.ID_R)):
+                        inkex.errormsg('The ID is already in use, please change it to another number.')
+                    else: 
+                        add_inverse_reaction(self, self.options.ID_R, self.options.KEGG_reaction_R, self.options.KEGG_enzime_R, self.options.x_R, self.options.y_R, self.options.size_R)
 
 if __name__ == '__main__':
     Constructor().run()
